2c46832c6b2e5eb67ab7726ef623a50fd47310b9
Alexis Vasquez <avasquez@MBP-de-Alexis.home>
2017-07-15 01:52:01 -0400

    alexis

--- empleados.php
+++ empleados.php
@@ -6,7 +6,7 @@
 		exit;
         }
 
-	/* Connect To Database*/
+	/* Connect To Databasehhh*/
 	require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 	require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
 		$active_empleados="active";	
